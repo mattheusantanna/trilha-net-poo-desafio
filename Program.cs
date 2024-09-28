0262bb42d1073e27c75f9f29647b346dbfe86739
mattheusantanna <mattheusantanna@outlook.com>
2024-09-28 13:47:20 -0300

    Resolução dos TODOS

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia1 = new Nokia("13981325972", "Nokia", "5620", 128);
+Console.WriteLine("Smartphone Nokia:");
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone1 = new Iphone("1397340136", "Iphone15", "2056", 256);
+Console.WriteLine("Smartphone Iphone:");
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Telegram");
